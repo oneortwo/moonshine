7e6d73cd3526315a0d1974aa06f5af7c1c221d9c
Theodor Sjöstedt <theodor.sjostedt@gmail.com>
2012-12-20 23:54:43 +0100

    some comments

--- moonshine.js
+++ moonshine.js
@@ -1,10 +1,13 @@
 var connect = require('connect');
 var fs = require('fs');
 
+// web server starts here
 connect.createServer(
     connect.static(__dirname)
 ).listen(8080);
+// ...and ends here...
 
+// this part is only to test the file writing :)
 fs.writeFile("/tmp/test", "Hey there!", function(err) {
     if(err) {
         console.log(err);
